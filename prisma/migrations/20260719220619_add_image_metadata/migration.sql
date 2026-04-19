898944d037be5ee6bcd9c092f382992d49370565
Mikita Pilets <mykytapilec@gmail.com>
2026-04-19 18:00:00 +0200

    feat: add image processing metadata

---
+++ prisma/migrations/20260719220619_add_image_metadata/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "images" ADD COLUMN     "format" TEXT,
+ADD COLUMN     "height" INTEGER,
+ADD COLUMN     "width" INTEGER;
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,4 +1,5 @@
 import { prisma } from '../database/prisma.js';
+import { ImageProcessingService } from '../processing/image-processing.service.js';
 import { FileStorageService } from '../storage/storage.service.js';
 import type { UploadedImage } from './image.types.js';
 
@@ -12,9 +13,14 @@ interface SaveImageInput {
 export class ImageService {
   constructor(
     private readonly storage: FileStorageService,
+    private readonly processing: ImageProcessingService,
   ) {}
 
   async create(input: SaveImageInput): Promise<UploadedImage> {
+    const metadata = await this.processing.getMetadata(
+      input.buffer,
+    );
+
     await this.storage.save({
       filename: input.filename,
       buffer: input.buffer,
@@ -25,6 +31,9 @@ export class ImageService {
         filename: input.filename,
         mimetype: input.mimetype,
         size: input.size,
+        width: metadata.width,
+        height: metadata.height,
+        format: metadata.format,
       },
     });
 
